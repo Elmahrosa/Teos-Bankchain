6d5cffc8e732c0605eff43db77b1211e1209e59f
Ayman Seif <92527473+Aymanseif@users.noreply.github.com>
2025-12-03 14:11:49 +0200

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.teos.bankchain",
+  appName: "Teos Bankchain",
+  webDir: "dist",
+  bundledWebRuntime: false
+};
+
+export default config;
